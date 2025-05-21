ca95f60f79af4b0728f3d52c26eb7c099bd4760c
coleexz <williamstevencole@gmail.com>
2025-05-21 15:13:22 -0600

    Installed vite and tailwind

---
+++ np/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 h-screen w-screen flex items-center justify-center">
+      <h1 className="text-white">HOLA PROYECTO DE ANALISIS</h1>
+    </div>
+  );
+}
+
+export default App;
