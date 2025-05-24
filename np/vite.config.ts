b1d086e9e2a7275ec126787ac3163e61bb171333
coleexz <williamstevencole@gmail.com>
2025-05-23 20:27:32 -0600

    Added @ to paths

--- np/vite.config.ts
+++ np/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(process.cwd(), "src") },
+      {
+        find: "@assets",
+        replacement: path.resolve(process.cwd(), "src/assets"),
+      },
+    ],
+  },
 });
